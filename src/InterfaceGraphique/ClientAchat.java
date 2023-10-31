5df36c3098e9405458ae50a1ea1ba148b3d47eda
Josué Castellani <josue.castellani@student.hepl.be>
2023-10-31 13:17:51 +0100

    311013H17

--- src/InterfaceGraphique/ClientAchat.java
+++ src/InterfaceGraphique/ClientAchat.java
@@ -1,6 +1,9 @@
 package InterfaceGraphique;
 
 import Properties.PropertiesPerso;
+import Socket.socket;
+import com.formdev.flatlaf.FlatDarculaLaf;
+
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
@@ -8,9 +11,12 @@ import javax.swing.border.Border;
 import javax.swing.border.LineBorder;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.Socket;
 import javax.swing.table.DefaultTableCellRenderer;
 //import javax.swing.table.TableCellRenderer;
 
@@ -58,6 +64,144 @@ public class ClientAchat extends JFrame
     private JPanel ImagePanel;
 
 
+    public static final String SERVER_IP = "192.168.146.128";
+    public static final int SERVER_PORT = 50000;
+
+
+    private boolean logged = false;
+    //int sClient;
+    Socket sClient;
+
+    public String getNom()
+    {
+        String nom = NomTxt.getText();
+        return nom;
+    }
+
+    public String getMotDePasse()
+    {
+        String mdp = MDPtxt.getText();
+        return mdp;
+    }
+
+    public int isNouveauClientChecked()
+    {
+        if (nouveauCliChecbox.isSelected())
+        {
+            return 1;
+        }
+        return 0;
+    }
+
+    public static Socket ClientSocket()
+    {
+        Socket socket = new Socket();
+
+        try
+        {
+            socket.connect(new InetSocketAddress(SERVER_IP, SERVER_PORT));
+            System.out.println("Connecté au serveur.");
+
+            return socket;
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+
+    public boolean OVESP_Login(String user, String password, int NouveauClient, Socket sClient)
+    {
+        int off = 0;
+        boolean onContinue = true;
+        String requete, reponse;
+
+        try {
+            // Construction de la requête
+            requete = "LOGIN#" + user + "#" + password + "#" + NouveauClient;
+
+            // Envoi de la requête + réception de la réponse
+            PrintWriter out = new PrintWriter(sClient.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(sClient.getInputStream()));
+
+            // Envoi de la requête
+            out.println(requete);
+
+            // Attente de la réponse
+            reponse = in.readLine();
+
+            if (reponse == null) {
+                System.out.println("Serveur arrêté, aucune réponse reçue...");
+                //sClient.close();
+                System.exit(1);
+            }
+
+            // Parsing de la réponse
+            String[] parts = reponse.split("#");
+            String statut = parts[1];
+
+            if ("ok".equals(statut)) {
+                String resultat = parts[2];
+                System.out.println("Résultat = " + resultat);
+                // Vous pouvez effectuer des opérations d'interface utilisateur ici
+
+                // Envoi d'une nouvelle requête CONSULT#1
+                requete = "CONSULT#1";
+                out.println(requete);
+                reponse = in.readLine();
+
+                if (reponse == null) {
+                    System.out.println("Serveur arrêté, aucune réponse reçue...");
+                    //sClient.close();
+                    System.exit(1);
+                }
+
+                String action = reponse.split("#")[1];
+                if ("ok".equals(action)) {
+                    int id, stock;
+                    String intitule, image;
+                    float prix;
+
+                    id = Integer.parseInt(reponse.split("#")[2]);
+                    intitule = reponse.split("#")[3];
+                    prix = Float.parseFloat(reponse.split("#")[4].replace(',', '.'));
+                    stock = Integer.parseInt(reponse.split("#")[5]);
+                    image = reponse.split("#")[6];
+
+                    System.out.println("id = " + id);
+                    System.out.println("intitule = " + intitule);
+                    System.out.println("prix = " + prix);
+                    System.out.println("stock = " + stock);
+                    System.out.println("image = " + image);
+
+                    // Vous pouvez effectuer des opérations d'interface utilisateur ici
+                } else {
+                    String erreur = reponse.split("#")[2];
+                    System.out.println("Erreur: " + erreur);
+                    onContinue = false;
+                }
+            } else {
+                String erreur = parts[2];
+                System.out.println("Erreur: " + erreur);
+                if (erreur.equals("Mot de passe incorrecte !")) {
+                    off++;
+                    // Vous pouvez effectuer des opérations d'interface utilisateur ici
+                    if (off == 3) {
+                        onContinue = false;
+                    }
+                } else {
+                    // Vous pouvez effectuer des opérations d'interface utilisateur ici
+                    onContinue = false;
+                }
+            }
+        } catch (IOException e) {
+            System.err.println("Erreur de communication avec le serveur : " + e.getMessage());
+            onContinue = false;
+        }
+
+        return onContinue;
+    }
 
     public ClientAchat()
     {
@@ -109,7 +253,7 @@ public class ClientAchat extends JFrame
 
 
         DefaultTableCellRenderer headerRenderer = new DefaultTableCellRenderer();
-        headerRenderer.setBackground(Color.PINK);
+        headerRenderer.setBackground(Color.BLACK);
         // Appliquez la couleur aux header
         for (int i = 0; i < model.getColumnCount(); i++)
         {
@@ -155,11 +299,46 @@ public class ClientAchat extends JFrame
         setIconImage(logoIcon.getImage());
 
 
+        LoginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                char[] login = new char[50];
+                char[] mdp = new char[50];
+                int nouveauClient = isNouveauClientChecked();
+
+                String user = getNom();
+                String password = getMotDePasse();
+
+
+
+
+                try
+                {
+                    sClient = ClientSocket();
+
+                    System.out.println("Connecté au serveur.");
+
+                    if (!OVESP_Login(user, password, nouveauClient, sClient)) {
+                        System.err.println("Erreur dans OVESP_Login");
+                        System.exit(1);
+                    }
+                } catch (Exception e2)
+                {
+                    System.err.println("Erreur de ClientSocket : " + e2.getMessage());
+                    System.exit(1);
+                }
+
+
+                logged = true;
+            }
+        });
 
 
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        FlatDarculaLaf.install(new FlatDarculaLaf());
         ClientAchat dialog = new ClientAchat();
         dialog.setVisible(true);
     }
---
+++ src/Socket/socket.java
@@ -0,0 +1,10 @@
+package Socket;
+
+
+
+public class socket
+{
+
+
+
+}
